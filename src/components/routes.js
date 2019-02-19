84aa0c522b1eb93af4dae9f831d9304b294c8b09
bandarub <bandaru.sravanthi@gmail.com>
2019-02-19 11:48:42 +0200

    deploying

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Form from './form';
 import Home from './home';
@@ -8,13 +8,13 @@ const Routing = props => {
 	const { contacts, getSelectedContact } = props;
 	return (
 		<div>
-			<Router>
+			<HashRouter>
 				<div>
 					<Route exact path="/" render={props => <Home {...props} contacts={contacts} />} />
 					<Route exact path="/newContact" render={props => <Form {...props} contacts={contacts} getSelectedContact={getSelectedContact} action="add"/>} />
 					<Route exact path="/:id/edit" render={props => <Form {...props} getSelectedContact={getSelectedContact} action="edit"/>} />
 				</div>
-			</Router>
+			</HashRouter>
 		</div>
 	);
 };
