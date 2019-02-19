0bee3b5d5cd19386fd83b2a718a278d6050c612a
bandarub <bandaru.sravanthi@gmail.com>
2019-02-19 11:09:43 +0200

    Updates

---
+++ precache-manifest.a22640aad07a90d645b805681de74618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d3e672b5b2346087d7",
+    "url": "/bandarub/contactbook/static/js/runtime~main.87d3e672.js"
+  },
+  {
+    "revision": "0e5bc0f49a371db595db",
+    "url": "/bandarub/contactbook/static/js/main.0e5bc0f4.chunk.js"
+  },
+  {
+    "revision": "9a7ea2de625cc1c20bd5",
+    "url": "/bandarub/contactbook/static/js/2.9a7ea2de.chunk.js"
+  },
+  {
+    "revision": "0e5bc0f49a371db595db",
+    "url": "/bandarub/contactbook/static/css/main.a1ff435c.chunk.css"
+  },
+  {
+    "revision": "fa00ef2cbbb1d93ef1e561b599f1f435",
+    "url": "/bandarub/contactbook/index.html"
+  }
+];
